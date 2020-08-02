ff70324a34e8c673ef10f3b2787613315bab0d45
Animish Pande <63346282+animishpande@users.noreply.github.com>
2020-08-02 16:56:57 +0530

    Initial commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Heading from "./Heading";
+import Lists from "./Lists";
+
+function App() {
+  return <div>
+    <Heading />
+    <Lists />
+  </div>;
+};
+
+export default App;
---
+++ src/Components/Lists.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Lists() {
+  return <ul>
+  <li>Pizza</li>
+  <li>Burger</li>
+  <li>Paneer</li>
+</ul>;
+};
+
+export default Lists;
